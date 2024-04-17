4dec49bd03a3e230015c33743debf6b27006f3bc
AKS <aks@AKSs-MacBook-Air.local>
2024-04-17 20:50:14 +0530

    initial commit

---
+++ src/main/java/com/scalar/productservice/ProductServiceFakeStoreServiceApplication.java
@@ -0,0 +1,13 @@
+package com.scalar.productservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceFakeStoreServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceFakeStoreServiceApplication.class, args);
+	}
+
+}
